490f382ea2f4f3115f9d284fe0d00b5bf67028a5
Harry Flanagan <S00189591@mail.itsligo.ie>
2019-10-08 12:59:35 +0100

    Add project files.

---
+++ Labsheet 4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labsheet_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Teams> teams = new List<Teams>();
+            {
+                Teams SligoRovers = new Teams("Sligo Rovers");
+                Teams FinnHarps = new Teams("Finn  Harps");
+                Teams GalwayUnited = new Teams("Galway  United");
+                Teams DerryCity = new Teams("Derry  City");
+                Teams Dundalk = new Teams("Dundalk");
+
+                
+
+                teams.Add(SligoRovers);
+                teams.Add(FinnHarps);
+                teams.Add(GalwayUnited);
+                teams.Add(DerryCity);
+                teams.Add(Dundalk);
+                
+                
+                
+            }
+
+            Display(teams);
+
+        }
+      static void Display(List<Teams>teams)
+        {
+            Console.WriteLine("{0,-15}{1,-15}{2,-15}{3,-15}{4,-15}","NAME","POINTS","WINS","DRAWS","LOSSES","PLAYED");
+            foreach ( Teams team in teams)
+            {
+                Console.WriteLine(team.DisplayTeamTable());
+            }
+        }
+
+    }
+}
---
+++ Labsheet 4/Teams.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labsheet_4
+{
+    public enum Result {NumberOfWins, NumberOfDraws, NumberOfLoss}
+    class Teams
+    {
+        public string TeamName { get; private set; }
+        public int NumberOfWins { get; private set; }
+    public int NumberOfLosses { get; private set; }
+        public int NumberOfDraws { get; private set; }
+        public int NumberOfGames { get; private set; }
+
+        private int points;
+
+        public int Points
+        {
+            get
+            {
+                points = (NumberOfWins * 3) + (NumberOfDraws);
+                return points;
+            }
+        }
+
+        public Teams(string name)
+        {
+            TeamName = name;
+        }
+
+        public string DisplayTeamTable()
+        {
+            return string.Format($"{TeamName,-15}{Points,-15}{NumberOfWins,-15}{NumberOfDraws,-15}{NumberOfLosses,-15}{NumberOfGames,-15}");
+        }
+
+
+    }
+}
